6ce1f3e6330853deafd9c7a354febcc48421161f
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2021-09-15 10:43:49 +0900

    Update labeling code
    img to skeleton img
    folder 단위로 해줌

---
+++ img_to_ske_labeling.py
@@ -0,0 +1,53 @@
+import argparse
+import logging
+import time
+import os
+import cv2
+import numpy as np
+
+from tf_pose.estimator import TfPoseEstimator
+from tf_pose.networks import get_graph_path, model_wh
+import scripts.label_image as label_img
+import scripts.label_image_scene as label_img_scene
+
+logger = logging.getLogger('Pose_Action_and_Scene_Understanding')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+fps_time = 0
+address = os.getcwd()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='tf-human-action-classification')
+    parser.add_argument('--dir', type=str, required=True)
+    parser.add_argument('--show-process', type=bool, default=False,
+                        help='for debug purpose, if enabled, speed for inference is dropped.')
+    args = parser.parse_args()
+
+    logger.debug('initialization %s : %s' % ('mobilenet_thin', get_graph_path('mobilenet_thin')))
+    e = TfPoseEstimator(get_graph_path('mobilenet_thin'), target_size=(432, 368))
+
+    for (root, dirs, files) in os.walk(args.dir):
+        print('ske_' + root)
+        if not os.path.exists('ske_'+root):
+            os.makedirs('ske_'+root)
+        for file in files:
+            print(root + '\\' + file)
+            image = cv2.imread(root + '\\' + file)
+            logger.info('cam image=%dx%d' % (image.shape[1], image.shape[0]))
+
+            # count = 0
+
+            logger.debug('+image processing+')
+            logger.debug('+postprocessing+')
+            humans = e.inference(image, upsample_size=4.0)
+
+            logger.debug('+classification+')
+            # Getting only the skeletal structure (with white background) of the actual image
+            image = np.zeros(image.shape,dtype=np.uint8)
+            image.fill(255) 
+            image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+            cv2.imwrite('ske_' + root + '\\' + file, image)
